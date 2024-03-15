6185eddfb1741bf43701b095039872d3f0cd2e9e
Yaroslav <yaroslav.0029@gmail.com>
2024-03-15 14:05:16 +0100

    create project

---
+++ Ancient Lands/Ancient_LandsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ancient_LandsApp.swift
+//  Ancient Lands
+//
+//  Created by Yaroslav Chlek on 15.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct Ancient_LandsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
