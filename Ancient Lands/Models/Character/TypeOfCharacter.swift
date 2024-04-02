64d4740c0750b3317f90429b537e895b5b80ee38
Yaroslav <yaroslav.0029@gmail.com>
2024-04-02 22:48:19 +0200

    Update TypeOfCharacter.swift

--- Ancient Lands/Models/Character/TypeOfCharacter.swift
+++ Ancient Lands/Models/Character/TypeOfCharacter.swift
@@ -18,17 +18,17 @@ enum TypeOfCharacter: CaseIterable, Codable {
     func getCharacteristic() -> TypeOfCharacterStruct {
         switch self {
         case .knight:
-            return TypeOfCharacterStruct(name: "Knight", assetName: "Knight", characterEnhancement: ["+20 to attack indoors,","+20 to defense indoors,"], passiveSkills: ["+10 to melee weapon attack,","when striking with a weapon, there is a chance (10%) to make a critical hit (+30%)."], hp: 70, attack: 40, defense: 30, dexterity: 10, stealth: 20)
+            return TypeOfCharacterStruct(name: "Knight", assetName: "Knight", characterEnhancement: ["+20 to attack indoors,","+20 to defense indoors,"], passiveSkills: ["+10 to melee weapon attack,","when striking with a weapon, there is a chance (10%) to make a critical hit (+30%)."], hp: 70, attack: 5, defense: 5, dexterity: 10, stealth: 20)
         case .elf:
-            return TypeOfCharacterStruct(name: "Elf", assetName: "Elf", characterEnhancement: ["+10 to defense in forest terrain,", "+20 to attack in forest terrain."], passiveSkills: ["+10 to ranged weapon attack,", "when fired there is a chance that the used arrow will not be taken away."], hp: 40, attack: 30, defense: 30, dexterity: 40, stealth: 20)
+            return TypeOfCharacterStruct(name: "Elf", assetName: "Elf", characterEnhancement: ["+10 to defense in forest terrain,", "+20 to attack in forest terrain."], passiveSkills: ["+10 to ranged weapon attack,", "when fired there is a chance that the used arrow will not be taken away."], hp: 40, attack: 3, defense: 5, dexterity: 40, stealth: 20)
         case .wizard:
-            return TypeOfCharacterStruct(name: "Wizard", assetName: "Wizard", characterEnhancement: ["+10 to cave defense,", "+10 to attack in the cave."], passiveSkills: ["+10 to healing"], hp: 30, attack: 60, defense: 30, dexterity: 20, stealth: 20)
+            return TypeOfCharacterStruct(name: "Wizard", assetName: "Wizard", characterEnhancement: ["+10 to cave defense,", "+10 to attack in the cave."], passiveSkills: ["+10 to healing"], hp: 30, attack: 6, defense: 5, dexterity: 20, stealth: 20)
         case .knightWm:
-            return TypeOfCharacterStruct(name: "Knight", assetName: "KnightWm", characterEnhancement: ["+20 to attack indoors,","+20 to defense indoors,"], passiveSkills: ["+10 to melee weapon attack,","when striking with a weapon, there is a chance (10%) to make a critical hit (+30%)."], hp: 70, attack: 40, defense: 30, dexterity: 10, stealth: 20)
+            return TypeOfCharacterStruct(name: "Knight", assetName: "KnightWm", characterEnhancement: ["+20 to attack indoors,","+20 to defense indoors,"], passiveSkills: ["+10 to melee weapon attack,","when striking with a weapon, there is a chance (10%) to make a critical hit (+30%)."], hp: 70, attack: 5, defense: 5, dexterity: 10, stealth: 20)
         case .elfWm:
-            return TypeOfCharacterStruct(name: "Elf", assetName: "ElfWm", characterEnhancement: ["+10 to defense in forest terrain,", "+20 to attack in forest terrain."], passiveSkills: ["+10 to ranged weapon attack,", "when fired there is a chance that the used arrow will not be taken away."], hp: 40, attack: 30, defense: 30, dexterity: 40, stealth: 20)
+            return TypeOfCharacterStruct(name: "Elf", assetName: "ElfWm", characterEnhancement: ["+10 to defense in forest terrain,", "+20 to attack in forest terrain."], passiveSkills: ["+10 to ranged weapon attack,", "when fired there is a chance that the used arrow will not be taken away."], hp: 40, attack: 3, defense: 5, dexterity: 40, stealth: 20)
         case .wizardWm:
-            return TypeOfCharacterStruct(name: "Wizard", assetName: "WizardWm", characterEnhancement: ["+10 to cave defense,", "+10 to attack in the cave."], passiveSkills: ["+10 to healing"], hp: 30, attack: 60, defense: 30, dexterity: 20, stealth: 20)
+            return TypeOfCharacterStruct(name: "Wizard", assetName: "WizardWm", characterEnhancement: ["+10 to cave defense,", "+10 to attack in the cave."], passiveSkills: ["+10 to healing"], hp: 30, attack: 6, defense: 5, dexterity: 20, stealth: 20)
         }
     }
 }
